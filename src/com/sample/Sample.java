f4ffefab8c2c5e936db59646275520547f29eb71
Jeba1995 <jeba.angel.95@gmail.com>
2023-10-19 21:48:46 +0530

    new code

---
+++ src/com/sample/Sample.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+public class Sample {
+		public int a;
+		public void sample() {
+			System.out.println(a);
+			System.out.println("Sample");
+		}
+		public static void main(String[] args) {
+			Sample l=new Sample();
+			l.sample();
+		}
+	}
+
